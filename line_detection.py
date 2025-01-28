26f4da232c19a87ccaf216356723e7ff1792e460
Cao Yang <yc620@cam.ac.uk>
2025-01-28 11:22:36 +0000

    Updated and merged files

--- line_detection.py
+++ line_detection.py
@@ -2,6 +2,7 @@ from machine import Pin
 import time
 import threading
 from motor import *
+from PID_control import *
 
 p_ll = Pin(1, Pin.In) # Input pin for left most sensor
 p_l = Pin(2, Pin.In) # Input pin for left sensor
@@ -13,7 +14,7 @@ light = Pin(5, Pin.Out) # Output pin for light
 def blinking(flag):
     if flag == True:
         light.value(not light.value())
-        sleep(0.5)
+        time.sleep(0.5)
 
 
 
@@ -24,6 +25,16 @@ motor_right = Motor_right()
 
 start_time = time.time()
 
+
+
+
+# Initialize PID
+pid = PIDController(Kp=1.0, Ki=0.1, Kd=0.05)
+
+# Main control loop
+base_speed = 50  # Base speed of the robot
+last_time = time.time()
+
 #Put a timer of 4.5 mins
 while (time.time() - start_time < 270): # 4.5 mins
 
@@ -31,7 +42,19 @@ while (time.time() - start_time < 270): # 4.5 mins
     # Lighting
     threading.Timer(0, blinking(flag)).start() # need to set flag when vehicle leaves box
     # Main loop here, with control theory
+    sensor_value = [p_ll.value(), p_l.value(), p_r.value(), p_rr.value()]
+    pid.sensor_values = sensor_value
 
+    error = pid.calculate_error()
+
+    current_time = time.time()
+    dt = current_time - last_time
+    correction = pid.compute(error, dt)
+    last_time = current_time
+    #Time keeping to calculate dt and correction
+
+    # Adjust motor speeds
+    left_speed, right_speed = pid.motor_speed(base_speed, correction)
 
 
 
