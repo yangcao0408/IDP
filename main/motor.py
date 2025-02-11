7658e9817d22fc24d1c7f6eca846fc6408498f27
Goachnut <157056281+Goachnut@users.noreply.github.com>
2025-02-11 09:46:28 +0000

    Update motor.py
    Added commenting

--- main/motor.py
+++ main/motor.py
@@ -1,4 +1,5 @@
 from machine import Pin, PWM
+#This module defines each motor as a class, setting up speed setting as a method and pinouts in the initialisation of each class
 
 class Motor_left:
  def __init__(self):
@@ -6,16 +7,21 @@ class Motor_left:
     self.pwm1 = PWM(Pin(6))
     self.pwm1.freq(1000)
     self.pwm1.duty_u16(0)
+    #This block initialises pins for direction and pwm control
+    #PWM frequency is set to 1kHz and the PWM duty cycles initialised to 0 to start the motor as off
  def off(self):
     self.pwm1.duty_u16(0)
+    #This method sets PWM duty cycle to 0, turning off the motor
  def speed_change(self, speed, direction):
     self.pwm1.duty_u16(int(65535*speed/100))
     self.m1Dir.value(direction)
     #direction: forward = 0, reverse = 1
+    #This method scales the PWM duty cycle up to its maximum according to the percentage given in the speed input, and also sets the direction of the motor
 
+#The following class is the same as that above but adjusted for the opposite motor
 class Motor_right:
  def __init__(self):
-    self.m2Dir = Pin(4 , Pin.OUT) # set pin left wheel
+    self.m2Dir = Pin(4 , Pin.OUT) # set pin right wheel
     self.pwm2 = PWM(Pin(5))
     self.pwm2.freq(1000)
     self.pwm2.duty_u16(0)
@@ -24,3 +30,4 @@ class Motor_right:
  def speed_change(self, speed, direction):
     self.pwm2.duty_u16(int(65535*speed/100))
     self.m2Dir.value(direction)
+    #direction: forward = 0, reverse = 1
