3b5989b64addfbb58b5ff194e84e74bf5e639209
Goachnut <157056281+Goachnut@users.noreply.github.com>
2025-02-11 10:07:14 +0000

    Update pathblocks.py
    Added comments

--- main/pathblocks.py
+++ main/pathblocks.py
@@ -4,23 +4,25 @@ import time
 from motor import *
 from PID_control import *
 
+#The goal of pathblocks is to segment and systematise stages of the path into reuseable standard elements
+#These elements can then be followed one after another to form a given path
+
 def followline_until(pid, trigger, action, motor_left, motor_right, base_speed, duration):
+  #This function takes a trigger and action and will follow the straight line until a given trigger is detected, then will perform the chosen action e.g. follow the line until a left junction then turn left
+  
+  #This section corresponds chosen triggers to their sensor conditions
   if trigger == "left_junction":
     trigger_value = lambda: pid.sensor_values[0] * pid.sensor_values[1] * (1 - pid.sensor_values[3])
+    #trigger_value will return 1 when the trigger has been detected, for example here left_junction trigger is LL * L * NOT(RR), i.e. both left side sensors are on but rightmost is off
+    #lambda function has been used here to keep trigger_value as a continually updating dynamic variable rather than a static variable
   elif trigger == "right_junction":
     trigger_value = lambda: ((1 - pid.sensor_values[0]) * pid.sensor_values[2] * pid.sensor_values[3])
   elif trigger == "t_junction":
     trigger_value = lambda: pid.sensor_values[0] * pid.sensor_values[3]
-  #trigger_value has value 1 when it is activated
-  
-  iter = 0
+
+  #The following loops line following until the trigger is detected
   while trigger_value() != 1:
     pid.detect_sensor()
-
-    if iter == 200:
-      print(left_speed, right_speed)
-      iter = 0
-    iter += 1
 
     error = pid.error_calc()
     dt = 0.001
@@ -31,15 +33,18 @@ def followline_until(pid, trigger, action, motor_left, motor_right, base_speed, 
     motor_right.speed_change(speed = right_speed, direction = right_dir)
     time.sleep(0.001)
 
+  #After the trigger is detected, whichever action was chosen is then executed
   if action == "turn_left":
     pid.turn_left_90(duration)
   elif action == "turn_right":
     pid.turn_right_90(duration)
   elif action == "forward":
+    #This action effectively acts as 'ignore', i.e. upon seeing a junction this action will continue driving straight forwards
     motor_left.speed_change(speed = 90, direction = 0)
     motor_right.speed_change(speed = 90, direction = 0)
     time.sleep(0.5)
-  # Reinitialise
+
+  #Reinitialise values
   motor_left.speed_change(speed = 90, direction = 0)
   motor_right.speed_change(speed = 90, direction = 0)
   pid.sensor_values = [0, 0, 0, 0]
